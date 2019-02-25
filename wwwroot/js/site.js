c30ab63a2a8221673e3dce3529882607a86bd2d4
MattHelton <mjoelhelton@me.com>
2019-02-24 21:59:36 -0500

    Imports jquery-ui-min.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$( function($) {
+    $( "#dialog-message" ).dialog({
+      modal: true,
+      buttons: {
+        Ok: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  } );
+ 
